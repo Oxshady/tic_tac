4b3c25ccbf896f2626fa866ea0c240d631858ece
Oxshady <shadymahmod55@gmail.com>
2024-08-30 14:40:43 +0300

    everything is microservice ^_^

--- main.py
+++ main.py
@@ -1,63 +1,19 @@
 # #!/usr/bin/python3
 from models import db
-db.create_all()
-from models.model import User, Ranks
-
-x = db.get_session()
-rank = Ranks(win=5, loss=2, draw=1 ,user_id="066d442d4db7442f9a9b928631f7eca2")
-rank = Ranks(win=5, loss=2, draw=1 ,user_id="066d442d4db7442f9a9b928631f7eca2")
-db.save(rank)
-db.update()
-user = x.query(User).filter_by(id="066d442d4db7442f9a9b928631f7eca2").first()
-print(user.ranks.win)
-# from models.user_model import User
-# from models import db
-# from views.signup import signup
-# from views.login import login
-# from flask import Flask, g
-# from models.user_model import Rank
-# if __name__ == '__main__':
-#     app = Flask(__name__)
-#     app.config["SECRET_KEY"] = "jadsnjkndsjkandjknjkasndjkansjkn adjk"
-#     app.register_blueprint(signup)
-#     app.register_blueprint(login)
-#     @app.before_request
-#     def before_request():
-#         g.db_session = db.get_session()
-#     x = Rank(win="5", loss="2",  user_id="066d442d4db7442f9a9b928631f7eca2")
-#     db.save(x)
-#     db.update()
-#     @app.teardown_request
-#     def teardown_request(exception=None):
-#         db.remove_session()
-#     app.run(debug=True)
-# # from werkzeug.security import generate_password_hash
-# # from uuid import uuid4
-# # from datetime import datetime
-# # from flask import Flask, g
-# # if __name__ == '__main__':
-# #     app = Flask(__name__)
-# #     app.config["SECRET_KEY"] = "jadsnjkndsjkandjknjkasndjkansjkn adjk"
-# #     app.register_blueprint(signup)
-# #     app.register_blueprint(login)
-# #     @app.before_request
-# #     def before_request():
-# #         g.db_session = db.get_session()
-
-# #     @app.teardown_request
-# #     def teardown_request(exception=None):
-# #         db.remove_session()
-# #     app.run(debug=True)
-
-# # common_password = 'easy_password'
-# # hashed_password = generate_password_hash(common_password, method='pbkdf2:sha256')
-# # for i in range(1, 101):
-# #     user_id = uuid4().hex
-# #     username = f'user{i}'
-# #     email = f'user{i}@example.com'
-# #     created_at = datetime.utcnow().isoformat()
-# #     updated_at = created_at
-# #     user = User(**{"id":user_id, "username":username, "email":email, "password":hashed_password, "created_at":created_at, "updated_at":updated_at})
-# #     db.save(user)
-
-# # print("Inserted 100 user records into the users table.")
+from views.signup import signup
+from views.login import login
+from views.ranking import ranking
+from flask import Flask, g
+if __name__ == '__main__':
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "jadsnjkndsjkandjknjkasndjkansjkn adjk"
+    app.register_blueprint(signup)
+    app.register_blueprint(login)
+    app.register_blueprint(ranking)
+    @app.before_request
+    def before_request():
+        g.db_session = db.get_session()
+    @app.teardown_request
+    def teardown_request(exception=None):
+        db.remove_session()
+    app.run(debug=True)
--- views/ranking.py
+++ views/ranking.py
@@ -1,45 +1,31 @@
-from flask import Flask, jsonify, render_template, request
-from flask_cors import CORS
+from models import db
+from flask import jsonify, Blueprint, request,g
+from models.model import Ranks
 
-app = Flask(__name__)
-CORS(app)
+ranking = Blueprint('ranking',__name__)
 
-rankings = [
-    {"id": 1, "name": "Ali", "win": 5, "loss": 2, "draw": 0},
-    {"id": 2, "name": "Fatima", "win": 4, "loss": 3, "draw": 1},
-    {"id": 3, "name": "Mohammed", "win": 6, "loss": 1, "draw": 1},
-    {"id": 4, "name": "Sarah", "win": 5, "loss": 3, "draw": 2},
-    {"id": 5, "name": "Youssef", "win": 4, "loss": 4, "draw": 2},
-]
-
-for i in rankings:
+rankings = db.all(Ranks)
+rankings_list = [ i.to_dict() for i in rankings]
+for i in rankings_list:
     i['score'] = i.get('win')*3 + i.get('draw')*1 - i.get('loss')*2
 
-
-@app.route('/api/ranking/<int:ranking_id>', methods=['GET'])
+@ranking.route('/api/ranking/<ranking_id>', methods=['GET'])
 def get_ranking(ranking_id):
-    for search_rank in rankings:
+    for search_rank in rankings_list:
         if search_rank.get('id') == ranking_id:
             return jsonify(search_rank)
     return jsonify({}), 404
 
-@app.route('/api/ranking', methods=['GET','PUT'])
+@ranking.route('/api/ranking', methods=['GET','PUT'])
 def get_rankings():
+    ranking_sorted = None
     sort_by = request.args.get('sort_by')
     if sort_by == 'win':
-        ranking_sorted = sorted(rankings, key=lambda x: x['win'])
+        ranking_sorted = sorted(rankings_list, key=lambda x: x['win'])
     elif sort_by == 'loss':
-        ranking_sorted = sorted(rankings, key=lambda x: x['loss'])
+        ranking_sorted = sorted(rankings_list, key=lambda x: x['loss'])
     elif sort_by == 'draw':
-        ranking_sorted = sorted(rankings, key=lambda x: x['draw'])
+        ranking_sorted = sorted(rankings_list, key=lambda x: x['draw'])
     elif sort_by == 'score':
-        ranking_sorted = sorted(rankings, key=lambda x: x['score'])
+        ranking_sorted = sorted(rankings_list, key=lambda x: x['score'])
     return jsonify(ranking_sorted)
-
-@app.route('/')
-def index():
-    """Render the HTML page."""
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
