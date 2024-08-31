38539cdaa1bcdc5a3379c9b02bb01fc285bbb475
Oxshady <shadymahmod55@gmail.com>
2024-08-31 14:52:51 +0300

    ^_^

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/bash
-echo "Starting Flask server..."
+echo "install requirments of flask"
+pip3 install -r req.txt
+echo "install requirment of node"
+cd static
+npm install
+cd ..
+echo "starting Flask server..."
 python3 main.py &
-echo "Starting React app..."
+echo "starting React app..."
 cd static
 npm run dev &
 echo "Starting Node.js backend server..."
