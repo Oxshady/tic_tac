ae26f6bd204861ceae3864e1bd7f80aeab0f9c0c
Oxshady <shadymahmod55@gmail.com>
2024-08-31 11:27:59 +0300

    ^_^ production mode now

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ if __name__ == '__main__':
     def teardown_request(exception=None):
         db.remove_session()
     CORS(app, resources={r"/*": {"origins": "*"}})
-    app.run(debug=True, port=5000)
+    app.run(port=5000)
