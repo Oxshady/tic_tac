c33d94dfcae9e5a83cb97d735af0e25f81277a9d
Oxshady <shadymahmod55@gmail.com>
2024-09-05 02:12:18 +0300

    rank editing in signup

--- views/signup.py
+++ views/signup.py
@@ -4,7 +4,7 @@ signup = Blueprint('signup', __name__)
 @signup.route('/signup', methods=['POST'])
 def signup_user():
     from models import db
-    from models.model import User
+    from models.model import User, Ranks
     if request.method == 'POST':
         data = request.get_json()
         username = data.get('username')
@@ -14,5 +14,7 @@ def signup_user():
             return jsonify({"success":  False, "message": "please fill out all fields"}), 400
         user = User(**{"username":username, "password":password, "email":email})
         db.save(user)
+        rank = Ranks(user=user)
+        db.save(rank)
         return jsonify({"success":  True, "message": "signup successful"}), 200
     return jsonify({"success":  False, "message": "signup failed"}), 401
